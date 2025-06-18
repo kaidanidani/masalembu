345c366e9aa8a6f6867b6859ea3f3673cdb964db
Kaidani <135043059+kaidanidani@users.noreply.github.com>
2025-06-18 23:07:54 +0700

    baru

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -394,43 +394,7 @@ article h1 {
 
 
 /* Masalembu End */
-=======
 
-  .hero-section {
-  position: relative;
-  height: 400px; /* kamu bisa ubah ke 300px jika mau lebih pendek */
-  overflow: hidden;
-}
-
-.hero-img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  display: block;
-}
-
-.hero-overlay {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  padding: 1rem;
-  z-index: 1;
-}
-
-.hero-section::after {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.4); /* efek gelap */
-  z-index: 0;
-}
-
->>>>>>> kaidani
 </style>
 
 <!-- ///////////////////////// -->
